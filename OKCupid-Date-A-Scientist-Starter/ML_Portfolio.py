0e9d02d558e5ecb5ba83297870f055143b3319f3
GreatKingCody <Prototype2000.11@gmail.com>
2022-05-03 19:18:18 -0400

    Apparently I wrote a very intensive function

--- OKCupid-Date-A-Scientist-Starter/ML_Portfolio.py
+++ OKCupid-Date-A-Scientist-Starter/ML_Portfolio.py
@@ -24,7 +24,7 @@ from sklearn.preprocessing import MultiLabelBinarizer
 # Write in the csv
 df = pd.read_csv('profiles.csv')
 # print(df.info)
-
+df.fillna('did not answer', inplace = True)
 
 #Explore the columns
 # print(df.columns)
@@ -47,6 +47,7 @@ df.care = df.care.str.replace('and it\'s fun to think abo', 'it\'s fun to think 
 
 df.care = df.care.str.replace('nan', 'did not answer')
 
+
 df = df.convert_dtypes()
 # print(df.care.head())
 
@@ -58,21 +59,28 @@ df = df.convert_dtypes()
 df.body_type.replace(['a little extra', 'curvy', 'full figured', 'overweight'], 'above average', inplace = True)
 df.body_type.replace(['fit', 'jacked'], 'athletic', inplace = True)
 df.body_type.replace(['skinny', 'used up', 'thin'], 'below average', inplace = True)
-df.body_type = df.body_type.str.replace('<NA>', 'did not answer')
 df.body_type.replace('rather not answer', 'did not answer', inplace = True)
-df.body_type.fillna('did not answer', inplace = True)
+
 
 
 df.drinks.replace('desperately', 'very often', inplace = True)
-df.drinks.fillna('did not answer', inplace = True)
 
 
-df.drugs.fillna('did not answer', inplace = True)
+x = 'dropped out'
+def education_replacement(df, column):
+    for i in df[column]:
+        if 'college' in i and x not in i:
+            df[column] = df[column].replace(i, 'graduated from/enrolled in college', inplace = True)
+        elif 'med school' in i and x not in i:
+            df[column] = df[column].replace(i, 'graduated from/enrolled in med school', inplace = True)
+        elif 'high school' in i and x not in i:
+            df[column] = df[column].replace(i, 'graduated from/enrolled in high school', inplace = True)
+        else:
+            pass
+education_replacement(df, 'education')
+            
 
-
-print(df.drugs.unique())
-
-
+# print(df.education.unique())
 
 
 
