58d34a70a1f9617f9065ce2a0122632366e4db38
GreatKingCody <Prototype2000.11@gmail.com>
2022-05-03 16:24:00 -0400

    Having issues with my Python extension

--- OKCupid-Date-A-Scientist-Starter/ML_Portfolio.py
+++ OKCupid-Date-A-Scientist-Starter/ML_Portfolio.py
@@ -16,14 +16,14 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
-
+from sklearn.preprocessing import MultiLabelBinarizer
 
 
 
 
 # Write in the csv
 df = pd.read_csv('profiles.csv')
-# print(df.head())
+print(df.info)
 
 
 #Explore the columns
@@ -31,19 +31,12 @@ df = pd.read_csv('profiles.csv')
 # print(df.head(1))
 
 
-# print(df.sign.value_counts())
-sign = df.sign
 
-split1 = sign.str.split(' ', 1)
-
-signs = []
-matter = []
+# Binarise labels
+mlb = MultiLabelBinarizer()
+expandedLabelData = mlb.fit_transform(df["sign"])
+labelClasses = mlb.classes_
 
 
-for i in split1:
-    if type(i) == list and len(i) == 1:
-        signs.append(i)
-    elif type(i) == list and len(i) == 2:
-        matter.append(i)
-# print(signs)
-print(matter)
+# Create a pandas.DataFrame from our output
+expandedLabels = pd.DataFrame(expandedLabelData, columns=labelClasses)
